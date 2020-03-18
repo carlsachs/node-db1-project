5f79f31de445fcde062d147d6061acbaa059f28a
carlsachs <sachscarl@gmail.com>
2020-03-18 12:45:47 -0400

    updated server.js

--- api/server.js
+++ api/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const AccountRouter = require("../accounts/accounts-router.js")
 
 const db = require("../data/dbConfig.js");
 
 const server = express();
 
 server.use(express.json());
+
+server.use("/api/accounts", AccountRouter)
+
+server.get("/", (req, res) => {
+    res.status(200).json({ api: "Running" });
+});
 
 module.exports = server;
